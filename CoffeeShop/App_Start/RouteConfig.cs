5b028ada5b8c49201b7d40aeba0fa123ac677f31
LiorDaichman <liordaichman@gmail.com>
2021-12-09 20:01:15 +0200

    Home Page + Connect + Query

---
+++ CoffeeShop/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CoffeeShop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            routes.MapRoute(
+                name: "HomePage",
+                url: "",
+                defaults: new { controller = "HomePage", action = "ViewHomePage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "HomePage2",
+                url: "Home",
+                defaults: new { controller = "HomePage", action = "ViewHomePage", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Contact",
+                url: "Contact",
+                defaults: new { controller = "HomePage", action = "Contact", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+        }
+    }
+}
---
+++ CoffeeShop/Controllers/HomePageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult ViewHomePage()
+        {
+            return View("Home");
+        }
+
+        public ActionResult Contact()
+        {
+            return View("Contact");
+        }
+    }
+}
